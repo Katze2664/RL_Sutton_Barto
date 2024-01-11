bb5f4c7f635692e93344b0563d251cfe71ab796b
Katze2664 <40237250+Katze2664@users.noreply.github.com>
2024-01-11 17:28:53 +1100

    Refactored Agent into AgentActionValuerPolicy
    Created abstract base classes for Agent, ActionValuer and Policy, and the corresponding subclasses AgentActionValuerPolicy, SampleAverager and EpsilonGreedy, respectively.

--- main.py
+++ main.py
@@ -95,80 +95,170 @@ class EnvironmentBanditNonstationary(EnvironmentBandit):
 
 
 # %%
-class Agent:
-    """An epsilon-greedy agent that estimates action-values based on mean sampled rewards.
-    
-    Agent chooses a random action with probability eps, otherwise acts greedily,
-    choosing the action with the highest mean sampled reward. In the case of a tie
-    the action with the smallest index is chosen. Actions that have never been
-    sampled are estimated to have default_value."""
-    
-    def __init__(self, name, k, eps=0, default_value=0, stepsize=None):
+class Agent(ABC):
+    @abstractmethod
+    def __init__(self, name):
+        pass
+
+    @abstractmethod
+    def receive_observation(self, state_observed, reward):
+        pass
+
+    @abstractmethod
+    def output_action(self):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+
+class AgentActionValuerPolicy(Agent):
+    def __init__(self, name, action_valuer, policy):
         self.name = name
-        self.k = k
-        self.eps = eps
-        self.default_value = default_value
-        if stepsize is None:
-            self.stepsize = lambda action_count: 1 / action_count  # Equivalent to equal-weight sample averaging
-        else:
-            self.stepsize = stepsize
+        self.action_valuer = action_valuer
+        self.policy = policy
 
         self.reset()
-
+    
     def receive_observation(self, state_observed, reward):
         self.state_observed = state_observed
-        if self.action_selected is not None:
-            self.action_counts[self.action_selected] += 1
-            n = self.action_counts[self.action_selected]
-            q = self.action_values[self.action_selected]
-            self.action_values[self.action_selected] += self.stepsize(n) * (reward - q)
-
-            self.cumu_reward += reward
+        self.action_valuer.update_action_values(state_observed, self.action, reward)
 
     def output_action(self):
-        """Select eps greedy action"""
-        if random.random() < self.eps:
-            # Explore
-            self.action_selected = random.randrange(self.k)
-        else:
-            # Greedy
-            self.action_selected = np.argmax(self.action_values)
-        return self.action_selected
+        action_values = self.action_valuer.output_action_values()
+        self.action = self.policy.select_action(action_values, self.state_observed)
+        return self.action
 
     def reset(self):
-        self.action_selected = None
+        self.action = None
+        self.state_observed = None
+        self.action_valuer.reset()
+        self.policy.reset()
+    
+    def output_action_values(self, state=None, action=None):
+        return self.action_valuer.output_action_values(state, action)
 
-        self.action_counts = [0] * self.k
-        self.action_values = [self.default_value] * self.k
-        self.cumu_reward = 0
+
+class ActionValuer(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def update_action_values(self, state, action, reward):
+        pass
+
+    @abstractmethod
+    def output_action_values(self, state=None, action=None):
+        if state is None:
+            return self.action_values
+        elif action is None:
+            return self.action_values[state]
+        else:
+            return self.action_values[state][action]
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+class SampleAverager(ActionValuer):
+    def __init__(self, k, default_value=0, calc_stepsize=None):
+        self.k = k
+        self.default_value = default_value
+        if calc_stepsize is None:
+            self.calc_stepsize = lambda action_count: 1 / action_count  # Equivalent to equal-weight sample averaging
+        else:
+            self.calc_stepsize = calc_stepsize
+        self.reset()
+    
+    def update_action_values(self, state, action, reward):
+        if state not in self.action_values:
+            self.set_default_values(state)
+
+        if action is not None:
+            self.action_counts[state][action] += 1
+            n = self.action_counts[state][action]
+            q = self.action_values[state][action]
+            self.action_values[state][action] += self.calc_stepsize(n) * (reward - q)
+
+    def output_action_values(self, state=None, action=None):
+        return super().output_action_values(state, action)
+
+    def reset(self):
+        self.action_counts = {}
+        self.action_values = {}
+
+    def set_default_values(self, state):
+        self.action_counts[state] = [0] * self.k
+        self.action_values[state] = [self.default_value] * self.k
+
+
+class Policy(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def select_action(self, action_values, state=None):
+        pass
+    
+    @abstractmethod
+    def reset(self):
+        pass
+
+class EpsilonGreedy(Policy):
+    def __init__(self, eps=0):
+        self.eps = eps
+        self.reset()
+    
+    def select_action(self, action_values, state=None):
+        if state is not None:
+            action_values = action_values[state]
+        
+        if random.random() < self.eps:
+            # Explore
+            k = len(action_values)
+            action_selected = random.randrange(k)
+        else:
+            # Greedy
+            action_selected = np.argmax(action_values)
+        return action_selected
+
+    def reset(self):
+        pass
+
 
 # %%
 def run_simulation(max_rollouts, max_time_steps, environment, agents):
+    name_set = set()
+    for agent in agents:
+        assert agent.name not in name_set, "Agent names must be unique"
+        name_set.add(agent.name)
+
     cumu_reward_results = {agent.name: np.zeros((max_rollouts, max_time_steps)) for agent in agents}
     optimal_action_results = {agent.name: np.zeros((max_rollouts, max_time_steps)) for agent in agents}
-
+    
     for rollout in range(max_rollouts):
         environment.reset()
         for agent in agents:
             agent.reset()
 
         for time_step in range(max_time_steps):
-            environment.update_timestep(time_step)
+            environment.update_time_step(time_step)
             for agent in agents:
                 agent_name = agent.name
                 state_observed, reward = environment.output_observation(agent_name)
                 agent.receive_observation(state_observed, reward)
-
-                cumu_reward_results[agent.name][rollout, time_step] = agent.cumu_reward
-
+                
+                if time_step > 0:
+                    cumu_reward_results[agent.name][rollout, time_step] = cumu_reward_results[agent.name][rollout, time_step - 1] + reward
+                
                 action = agent.output_action()
                 environment.receive_action(agent_name, action)
                 
                 if action == environment.output_action_optimal():
                     optimal_action_results[agent.name][rollout, time_step] = 1
-
-
-    
     return cumu_reward_results, optimal_action_results
 # %%
 k = 10  # Number of actions the Agent can choose from
@@ -176,10 +266,12 @@ max_rollouts = 200  # Number of rollouts. Each rollout the Agent and Environment
 max_time_steps = 1000  # Number of time steps per rollout
 
 environment = EnvironmentBandit(k)
-agent00_0 = Agent("agent00_0", k, eps=0.00, default_value=0)
-agent01_0 = Agent("agent01_0", k, eps=0.01, default_value=0)
-agent10_0 = Agent("agent10_0", k, eps=0.10, default_value=0)
+# environment = EnvironmentBanditNonstationary(rand_walk_std=0.1, k=k)
+agent00_0 = AgentActionValuerPolicy("agent00_0", SampleAverager(k, default_value=0), EpsilonGreedy(eps=0))
+agent01_0 = AgentActionValuerPolicy("agent01_0", SampleAverager(k, default_value=0), EpsilonGreedy(eps=0.01))
+agent10_0 = AgentActionValuerPolicy("agent10_0", SampleAverager(k, default_value=0), EpsilonGreedy(eps=0.10))
 agents = [agent00_0, agent01_0, agent10_0]
+# agents = [agent10_0]
 
 # erwa = exponential recency-weighted averaging
 # agent10_0_erwa01 = Agent("agent10_0_erwa01", k, eps=0.10, default_value=0, stepsize=lambda n: 0.01)
@@ -196,7 +288,7 @@ colors = ["green", "red", "blue", "purple", "orange", "brown"]
 color_idx = 0
 
 for agent_name, cumu_reward in cumu_reward_results.items():
-    reward_time_mean = cumu_reward / (np.arange(max_time_steps) + 1)
+    reward_time_mean = cumu_reward / (np.arange(max_time_steps) + 1) # TODO: Convert to reward-per-step rather than time-averaged cumulative reward
     reward_rollout_mean = reward_time_mean.mean(axis=0)
     reward_rollout_std = reward_time_mean.std(axis=0, ddof=1)
     
